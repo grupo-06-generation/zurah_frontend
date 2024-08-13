6c3f6f9c1747ff7977b92503e80e87eceab578e7
Michel9406 <michel9406@hotmail.com>
2024-08-13 15:03:41 -0300

    :bricks:

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,28 @@ import Login from './pages/Login/Login';
 import About from './pages/About/About';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import Register from './pages/Register/Register';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
     <>
+    <AuthProvider>
+   
       <BrowserRouter>
         <Navbar />
         <div className='min-h-[80vh]'>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/register" element={<Register />} />
             <Route path="/home" element={<Home />} />
             <Route path="/sobre" element={<About />} />
           </Routes>
         </div>
         <Footer />
-      </BrowserRouter >
+      </BrowserRouter >   
+    </AuthProvider>
     </>
   );
 }
