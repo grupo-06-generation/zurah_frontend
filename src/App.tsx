86c3bbe1bafe120204409ba03ac311bd7a7880ae
PhysisOfWally <128063050+PhysisOfWally@users.noreply.github.com>
2024-08-13 11:34:21 -0300

    feat: Lógica de login implementada

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/cadastro" element={<Cadastro />} />
             <Route path="/home" element={<Home />} />
             <Route path="/sobre" element={<About />} />
           </Routes>
