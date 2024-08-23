6f9f42e60603b92f5559b30438efa32fecd20831
Michel9406 <michel9406@hotmail.com>
2024-08-23 10:45:36 -0300

    :dizzy:

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,30 +2,27 @@ import { Link } from "react-router-dom";
 import CarroselProduct from "../../components/Carrosel/CarroselProduct";
 import ListCategory from "../../components/Categories/ListCategory/ListCategory";
 import CategoryConsumer from "../../components/categoryConsumer/CategoryConsumer";
-//import ListProduct from "../../components/Products/ListProduct/ListProduct";
 import Rolagem from "../../components/Slider/Rolagem";
 import ListaProductHome from "@/components/Products/ListProduct/ListaProductHome";
 
 function Home() {
   return (
     <>
-    
-    <div className="min-h-screen bg-[#FFFBF7] flex flex-col items-center">
-      <main className="flex-grow w-full p-0">
-        <section id="home" className="mb-12">
-          <Rolagem />
-        </section>
-        <div className="flex-grow w-full p-14 flex flex-col items-center pb-6">
+      <div className="min-h-screen bg-[#FFFBF7] flex flex-col items-center">
+        <main className="flex-grow w-full p-0">
+          <section id="home" className="mb-12">
+            <Rolagem />
+          </section>
+          <div className="flex-grow w-full px-4 md:px-14 flex flex-col items-center pb-6">
+            <CategoryConsumer />
 
-          <CategoryConsumer />
-          
-          <ListaProductHome/>
+            <ListaProductHome />
 
-          <CarroselProduct />
-        </div>
-      </main>
-      <ListCategory />
-    </div>
+            <CarroselProduct />
+          </div>
+        </main>
+        <ListCategory />
+      </div>
     </>
   );
 }
