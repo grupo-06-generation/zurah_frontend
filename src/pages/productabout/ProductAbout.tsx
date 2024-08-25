12fbd9e64a9f35f7444aa47bf15f6f585633a147
maardgs <mariliarodrigues.94@gmail.com>
2024-08-25 19:49:20 -0300

    ajustando pagina produto

--- src/pages/productabout/ProductAbout.tsx
+++ src/pages/productabout/ProductAbout.tsx
@@ -8,19 +8,22 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { FaShoppingCart } from "react-icons/fa";
+import { FaShoppingCart, FaPlus, FaMinus } from "react-icons/fa";
 import { useNavigate, useParams } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "@/contexts/AuthContext";
 import { toastAlert } from "@/utils/toastAlert";
 import { buscar } from "@/services/Service";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Terminal } from "lucide-react";
 
 function ProductAbout() {
   const [product, setProduct] = useState<Product>({} as Product);
+  const [quantity, setQuantity] = useState(1);
+  const [totalPrice, setTotalPrice] = useState(0);
   const { id } = useParams<{ id: string }>();
   const navigate = useNavigate();
   const { adicionarProduto, authenticated } = useContext(AuthContext);
-
   const { usuario, handleLogout } = useContext(AuthContext);
   const token = usuario.token;
 
@@ -47,38 +50,90 @@ function ProductAbout() {
     }
   }, [id]);
 
+  useEffect(() => {
+    setTotalPrice(product.price * quantity);
+  }, [product.price, quantity]);
+
   function addToCart() {
     if (authenticated) {
-      adicionarProduto(product);
+      adicionarProduto({ ...product, quantity });
       toastAlert("Produto adicionado ao carrinho", "sucesso");
     } else {
       toastAlert("Você precisa estar logado", "error");
     }
   }
 
-  return (
-    <div className="flex justify-center">
-      <div className="">
-        <img src={product.photo} alt={product.name} />
-      </div>
+  function increaseQuantity() {
+    setQuantity((prev) => prev + 1);
+  }
 
-      <Card>
-        <CardHeader className="p-4">
-          <CardTitle>{product.name}</CardTitle>
-          <CardDescription className="line-clamp-2">
-            {product.description}
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="p-2">
-          <p className="text-xl font-semibold">R$ {product.price}</p>
-        </CardContent>
-        <CardFooter className="flex justify-center p-2 pb-4">
-          <Button className="bg-[#843c0a]" onClick={addToCart}>
-            <FaShoppingCart className="mr-2" />
-            Adicionar
-          </Button>
-        </CardFooter>
-      </Card>
+  function decreaseQuantity() {
+    if (quantity > 1) {
+      setQuantity((prev) => prev - 1);
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-center pt-20 pb-14">
+      <div className="w-full max-w-screen-lg flex flex-row">
+        <div className="flex-1 max-w-md rounded-lg">
+          <img
+            src={product.photo}
+            alt={product.name}
+            className="rounded-lg object-cover w-full h-full"
+          />
+        </div>
+        <div className="flex-1 mx-5">
+          <Card className="rounded-sm max-w-full pt-10 ">
+            <CardHeader className="p-4">
+              <CardTitle className="text-[#843C0A]">{product.name}</CardTitle>
+              <CardDescription className="line-clamp-2 pt-5">
+                {product.description}
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="p-2">
+              <p className="text-xl font-semibold">
+                R$ {totalPrice.toFixed(2)}
+              </p>
+            </CardContent>
+            <CardFooter className="flex justify-between items-center p-2 pb-4">
+              <div className="flex items-center">
+                <Button
+                  className="bg-gray-700 hover:bg-orange-500 "
+                  onClick={decreaseQuantity}
+                >
+                  <FaMinus />
+                </Button>
+                <span className="px-4 text-xl">{quantity}</span>
+                <Button
+                  className="bg-gray-700 hover:bg-orange-500 "
+                  onClick={increaseQuantity}
+                >
+                  <FaPlus />
+                </Button>
+              </div>
+              <Button
+                className="bg-[#843c0a] hover:bg-orange-500 ml-4"
+                onClick={addToCart}
+              >
+                <FaShoppingCart className="mr-2" />
+                Adicionar
+              </Button>
+            </CardFooter>
+          </Card>
+
+          <Alert className="mt-5 w-full max-w-screen-lg">
+            <Terminal className="mr-2" />
+            <AlertTitle>PRODUTO COM PESO VARIÁVEL</AlertTitle>
+            <AlertDescription>
+              O preço e a quantidade deste produto podem variar de acordo com o
+              peso médio. O valor indicado é uma estimativa, e a quantidade
+              final pode ser ajustada conforme o peso exato do produto no
+              momento da pesagem.
+            </AlertDescription>
+          </Alert>
+        </div>
+      </div>
     </div>
   );
 }
